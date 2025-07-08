35be552b169add08884fae948a1a94433901c80b
ARISA1115 <a.engine0808@gmail.com>
2025-07-08 13:37:55 +0900

    [chore] 未使用のimportをコメントアウトしCIテスト通過

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from slowapi.middleware import SlowAPIMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 
-import os
+# import os
 from app.api.api import router as api_router
 
 app = FastAPI()
@@ -15,7 +15,7 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
-# CORS設定 
+# CORS設定
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # 必要なら "http://localhost:3000" などに限定
@@ -24,12 +24,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def root():
     return {"status": "ok"}
 
+
 @app.get("/health")
 async def health_check():
     return {"status": "healthy", "service": "Hello, fastapi"}
+
 
 app.include_router(api_router, prefix="/api")
